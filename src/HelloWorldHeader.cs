640f4dd98260396b269bf3f627bf021fdee79b2a
unknown <derek@djdp.co>
2020-02-06 14:38:52 -0600

    Add code

---
+++ src/HelloWorldHeader.cs
@@ -0,0 +1,12 @@
+﻿using Sitecore.Pipelines.HttpRequest;
+
+namespace DJDP.HelloWorld
+{
+    public class HelloWorldHeader: HttpRequestProcessor
+    {
+        public override void Process(HttpRequestArgs args)
+        {
+            args.HttpContext.Response.Headers.Add("X-DOCKER-HEADER", "Hello World");
+        }
+    }
+}
